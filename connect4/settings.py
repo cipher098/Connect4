52458ff02d62e216bc56d3392ca8d1da7d106057
Saurabh Gothi <saurabhgothi@gmail.com>
2020-09-27 21:33:17 +0530

    fix: templates dir

--- connect4/settings.py
+++ connect4/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'connect4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": ["connect4/templates"],
+        "DIRS": ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
